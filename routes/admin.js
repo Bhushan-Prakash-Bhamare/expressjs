ef4b861b5fe11dc755e0795f231069e5f1df2e27
Bhushan-Prakash-Bhamare <Bhushubhamare@gmail.com>
2023-06-05 17:57:10 +0530

    Serving views

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,22 @@
 const express=require('express');
+const path=require('path');
+const fs=require('fs');
+
+const rootDir=require('../util/path');
 
 const router=express.Router();
 
 router.get('/add-product',(req,res,next)=>{
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>');
+    res.sendFile(path.join(rootDir,'views','add-product.html'));
 });
  
 router.post('/add-product',(req,res,next)=>{
     console.log(req.body)
     res.redirect('/')
+});
+
+router.use('/contactUs',(req,res,next)=>{
+    res.sendFile(path.join(rootDir,'views','contactUs.html'));
 });
 
 module.exports=router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,17 @@
 const express=require('express');
 
+const path=require('path');
+
+const rootDir=require('../util/path');
+
 const router=express.Router();
 
 router.get('/',(req,res,next)=>{
-    res.send('<h1>Hello from Express</h1>')
+    res.sendFile(path.join(rootDir,'views','shop.html'));
+});
+
+router.post('/success',(req,res,next)=>{
+    res.send('<h1>Form successfuly filled</h1>');
 });
 
 module.exports=router;
