8fed65d46863c6319f9153f834ea34621ea3e2c4
Bhushan-Prakash-Bhamare <Bhushubhamare@gmail.com>
2023-06-08 12:18:20 +0530

    app completed

--- models/cart.js
+++ models/cart.js
@@ -13,8 +13,9 @@ module.exports=class cart{
             let cart={ products:[],totalPrice:0};
             if(!err)
             {
-                cart=JSON.stringify(fileContent);
+                cart=JSON.parse(fileContent);
             }
+            console.log(cart);
             const existingProductIndex=cart.products.findIndex(prod=>prod.id===id);
             const existingProduct=cart.products[existingProductIndex];
             let updatedProduct;
@@ -35,4 +36,38 @@ module.exports=class cart{
             });
         });
     };
-}
+
+    static deleteProduct(id,productPrice){
+        fs.readFile(p,(err,fileContent)=>{
+            if(err){
+                return;
+            }
+            let cart=JSON.parse(fileContent);
+            const updatedCart={...cart};
+            const product=updatedCart.products.find(prod=>prod.id===id);
+            if(!product){
+                return;
+            }
+            const productQty=product.qty;
+            updatedCart.products=updatedCart.products.filter(prod=>prod.id!==id);
+            updatedCart.totalPrice=updatedCart.totalPrice-productPrice*productQty;
+            fs.writeFile(p,JSON.stringify(updatedCart),err=>{
+                console.log(err);
+            })
+
+        });
+    };
+
+    static getCart(cb){
+        fs.readFile(p,(err,fileContent)=>{
+            const cart=JSON.parse(fileContent);
+            if(err){
+                cb(null);
+            }
+            else{
+                cb(cart);
+            }
+        });
+    }
+};
+
